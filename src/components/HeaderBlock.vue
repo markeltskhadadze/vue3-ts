a75e9e568a8d740314c80a1f15112cf6af6dea34
markeltskhadadze <markeltskhadadze@gmail.com>
2023-07-25 15:36:13 +0400

    added route to admin

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+  import { useRouter } from "vue-router"
+
+  const router = useRouter()
+  function goToAdmin() {
+    router.push('/admin')
+  }
+
+</script>
+
 <template>
   <div class="flex justify-around items-center header-home">
     <div class="flex">
-      <p class="header-link me-3">Услуги</p>
+      <p class="header-link me-3" @click="goToAdmin">Услуги</p>
       <p class="header-link me-3">Тарифы</p>
       <p class="header-link me-3">Блог</p>
     </div>
