23fb8e2757409ef35b3adadda3a6caa76b69049e
markeltskhadadze <markeltskhadadze@gmail.com>
2023-08-09 18:10:52 +0400

    fix emdpoind

--- src/stores/send-form.ts
+++ src/stores/send-form.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const sendForm = defineStore('sendForm', () => {
     async function sendPhoneNumber (phoneNumber: string) {
         console.log(phoneNumber)
-        await axios.post('https://node-and-mongo-project.herokuapp.com/api/send-phone-number', {
+        await axios.post('https://node-and-mongo-project.herokuapp.com/api/send-phone', {
             phoneNumber
         })
     }
